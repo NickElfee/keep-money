ecf2c576afbcb29cd60e2b0e2d30ff18cb80c9ca
NickElfee <mysatovnick@gmail.com>
2019-06-26 00:40:58 +0300

    create animation in social-menu, fix position signIn

--- apps/keep/src/app/app.module.ts
+++ apps/keep/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {routing} from "./app.routing";
-import {ReactiveFormsModule} from "@angular/forms";
-import {TooltipModule} from "ngx-bootstrap";
-import {LoginComponent} from "./components/page/login/login.component";
-import {SignUpComponent} from "./components/smart/sign-up/sign-up.component";
+import { routing } from './app.routing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TooltipModule } from 'ngx-bootstrap';
+import { LoginComponent } from './components/page/login/login.component';
+import { SignUpComponent } from './components/smart/sign-up/sign-up.component';
+import { SignInComponent } from './components/smart/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
