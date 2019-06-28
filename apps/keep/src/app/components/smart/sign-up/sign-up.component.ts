d79ed44cacb14909acfa33bade382d7037991519
NickElfee <mysatovnick@gmail.com>
2019-06-29 01:52:34 +0300

    add new Url identify signIn and signUp

--- apps/keep/src/app/components/smart/sign-up/sign-up.component.ts
+++ apps/keep/src/app/components/smart/sign-up/sign-up.component.ts
@@ -1,23 +1,27 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { delay, takeUntil, tap } from 'rxjs/operators';
+import { UserService } from '../../../services/user.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {UserDto} from '../../../../../../../libs/data/src';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.sass']
+  styleUrls: ['./sign-up.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignUpComponent implements OnInit {
-  @Output() clickSingFormFull = new EventEmitter<UserDto>();
-    signUpFull(singUp: UserDto): void {
-      singUp = this.signUpFormFull.value;
-      this.clickSingFormFull.emit(singUp);
-    }
+export class SignUpComponent implements OnInit, OnDestroy {
+  @Input() logPassEmailForm: FormGroup;
 
   public signUpFormFull: FormGroup;
+  public isLoadedSignUp: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor(private builder: FormBuilder) { }
+  private onDestroy$ = new Subject<boolean>();
 
+  constructor(
+    private builder: FormBuilder,
+    private userService: UserService,
+  ) { }
 
   ngOnInit() {
     this.signUpFormFull = this.builder.group({
@@ -70,6 +74,30 @@ export class SignUpComponent implements OnInit {
         Validators.required
       ])
     });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next(true);
+    this.onDestroy$.complete();
+  }
+
+  public singUpFormFull(): void {
+    const userData = {
+      ...this.signUpFormFull.value,
+      ...{
+        password: this.logPassEmailForm.value.password,
+        email: this.logPassEmailForm.value.email,
+        login: this.logPassEmailForm.value.login,
+      }
+    };
+    this.userService.setUser(userData)
+      .pipe(
+        tap(() => this.isLoadedSignUp.next(true)),
+        delay(3000),
+        tap(() => this.isLoadedSignUp.next(false)),
+        takeUntil(this.onDestroy$),
+      )
+      .subscribe();
   }
 
   public onClickSignUpSubmit(): void {
